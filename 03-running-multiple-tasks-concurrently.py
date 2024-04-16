6487cdb04fdc29bcc3e7fe4bc15a31c7fe0b6db3
SALEH846 <ranaasaleh846@gmail.com>
2024-04-16 10:06:12 +0500

    Running multiple tasks concurrently

--- 03-running-multiple-tasks-concurrently.py
+++ 03-running-multiple-tasks-concurrently.py
@@ -2,11 +2,13 @@ import asyncio
 from util import delay
 
 async def main() -> None:
-    # All three tasks will concurrently, not one by one
+    # All the tasks will get scheduled, but not start running
     sleep_for_three = asyncio.create_task(delay(3))
     sleep_again = asyncio.create_task(delay(3))
     sleep_once_more = asyncio.create_task(delay(3))
 
+    # Once we hit first `await`, all the tasks which are registered start executing
+    # Because the event loop is started
     await sleep_for_three
     await sleep_again
     await sleep_once_more
