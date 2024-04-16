5babfaf6a6aefae6cb47db9638f930b40b229f64
SALEH846 <ranaasaleh846@gmail.com>
2024-04-16 10:13:29 +0500

    Running code while other operations complete

---
+++ 04-running-code-while-other-operations-complete.py
@@ -0,0 +1,17 @@
+import asyncio
+from util import delay
+
+async def hello_every_second() -> None:
+    for i in range(2):
+        await asyncio.sleep(1)
+        print(f"I'm running other code while I'm waiting!")
+
+async def main() -> None:
+    first_delay = asyncio.create_task(delay(3))
+    second_delay = asyncio.create_task(delay(3))
+
+    await hello_every_second()
+    await first_delay
+    await second_delay
+
+asyncio.run(main())
